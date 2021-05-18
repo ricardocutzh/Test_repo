26b337c6e0836f92b8d3be842e88c9eccd3f5e3e
ricardo <ricardo@nclouds.com>
2021-05-18 12:30:09 -0600

    adding slack tocken

---
+++ vars/ecsSharedPipeline.groovy
@@ -0,0 +1,25 @@
+def call (Map pipelineParams) {
+    pipeline {
+        agent {
+            node {
+                label 'master'
+            }
+        }
+        environment {
+            SSH_KEY = "test"
+        }
+        stages {
+            stage('Docker Build'){
+                steps {
+                    echo 'Docker build'
+                }
+            }
+        }
+
+        post {
+            always {
+                echo "cleaning up..."
+            }
+        }
+    }
+}
